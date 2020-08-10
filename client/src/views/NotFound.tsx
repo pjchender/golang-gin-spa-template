57fe4da617c4a513d93ebf86e70fb3c2fa9aaff0
aaronchen <aaronchen@jubo.health>
2020-08-10 18:36:23 +0800

    [BE] support static files folder and etag for cache-control

--- client/src/views/NotFound.tsx
+++ client/src/views/NotFound.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { RouteComponentProps } from '@reach/router';
 
 const NotFound: React.FC<RouteComponentProps> = () => {
-  return <h1 className="App-header">404 Not Found</h1>;
+  return <h1>404 Not Found</h1>;
 };
 
 export default NotFound;
--- server/main.go
+++ server/main.go
@@ -1,42 +1,53 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"log"
 	"net/http"
-	"path"
-	"path/filepath"
+	"strings"
 )
 
 func main() {
 	router := gin.Default()
 
-	// 讓所有 SPA 中的檔案可以在正確的路徑被找到
-	router.Use(static.Serve("/", static.LocalFile("./../client/build", true)))
+	// STEP 1：讓所有 SPA 中的檔案可以在正確的路徑被找到
+	router.Use(static.Serve("/", static.LocalFile("./../client/build", false)))
 
-	// API
+	// STEP 2： serve 靜態檔案
+	router.Static("/public", "./public")
+
+	// STEP 3：API
 	router.GET("/api", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
 
-	// 除了有定義路由的 API 之外，其他都會到前端框架
+	// STEP 4：除了有定義路由的 API 之外，其他都會到前端框架
+	// https://github.com/go-ggz/ggz/blob/master/api/index.go
 	router.NoRoute(func(ctx *gin.Context) {
-		dir, file := path.Split(ctx.Request.RequestURI)
+		file, _ := ioutil.ReadFile("./../client/build/index.html")
+		etag := fmt.Sprintf("%x", md5.Sum(file))
+		ctx.Header("ETag", etag)
+		ctx.Header("Cache-Control", "no-cache")
 
-		ext := filepath.Ext(file)
-		fmt.Printf("dir is %v; file is %v, ext is %v", dir, file, ext)
+		if match := ctx.GetHeader("If-None-Match"); match != "" {
+			if strings.Contains(match, etag) {
+				ctx.Status(http.StatusNotModified)
 
-		if file == "" || ext == "" {
-			// 如果網址最後沒有檔名或副檔名，則提供 SPA 的靜態網站首頁
-			ctx.File("./../client/build/index.html")
-		} else {
-			// 如果有檔案和檔名，則提供該檔案回去
-			ctx.File("./../client/public" + path.Join(dir, file))
+				return
+			}
 		}
+
+		ctx.Data(http.StatusOK, "text/html; charset=utf-8", file)
 	})
 
-	router.Run(":3000") // listen and serve on 0.0.0.0:3000
+	err := router.Run(":3000") // listen and serve on 0.0.0.0:3000
+	if err != nil {
+		log.Fatalln("Route can not be run", err)
+	}
 }
